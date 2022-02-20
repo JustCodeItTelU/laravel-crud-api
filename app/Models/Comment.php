98cf53df8e7294f9f2a6279e0e1162e1a9a9e5db
ingeniom22 <jamesmichael0444@gmail.com>
2022-02-20 19:10:52 +0700

    final

---
+++ app/Models/Comment.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['topic_id', 'comment'];
+
+    public function topic(){
+        return $this->belongsTo(Topic::class);}
+
+}
